1df5031e3dbca2080d146848e349145306ca83c9
Emre Samurlu <emrsmrl@gmail.com>
2022-05-03 23:11:22 +0300

    Tailwind Added

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      backgroundColor: "#141414",
+      primaryText: "#CFCFCF",
+      secondaryText: "#D53E30",
+      onGround: "#303030",
+      muted: "#8B8B8B",
+      borderColor: "#FFFFFF",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
