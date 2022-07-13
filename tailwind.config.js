979683b412605d0102b1cea063385477569aa938
Emre Samurlu <emrsmrl@gmail.com>
2022-07-13 23:59:40 +0300

    New Fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
 module.exports = {
-  content: [
+  mode: "jit",
+  purge: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./sections/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    colors: {
-      transparent: "transparent",
-      backgroundColor: "#141414",
-      primaryText: "#CFCFCF",
-      secondaryText: "#D53E30",
-      onGround: "#303030",
-      muted: "#8B8B8B",
-      borderColor: "#FFFFFF",
-    },
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
